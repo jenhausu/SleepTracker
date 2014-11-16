01342d0eb0ed6337deb6226c277727f8ba3bf34c
iOSBeginner <jenhausu@icloud.com>
2014-11-16 13:25:05 +0800

    Initialization

---
+++ SleepTracker/SleepTracker/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SleepTracker
+//
+//  Created by 蘇健豪1 on 2014/11/16.
+//  Copyright (c) 2014年 蘇健豪. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SleepTracker/SleepTracker/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SleepTracker
+//
+//  Created by 蘇健豪1 on 2014/11/16.
+//  Copyright (c) 2014年 蘇健豪. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
