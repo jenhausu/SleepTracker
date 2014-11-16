d3013c4deaa81b92195cc55c2536b3053bb02fc9
iOSBeginner <jenhausu@icloud.com>
2014-11-16 13:30:13 +0800

    Initialization

---
+++ SleepTracker/SleepTracker/HistoryViewController.h
@@ -0,0 +1,13 @@
+//
+//  HistoryViewController.h
+//  SleepTracker
+//
+//  Created by 蘇健豪1 on 2014/11/16.
+//  Copyright (c) 2014年 蘇健豪. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HistoryViewController : UIViewController
+
+@end
