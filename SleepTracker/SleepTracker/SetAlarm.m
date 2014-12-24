ac2b958453df28b1b66480421a63b9d9bf284e3c
iOSBeginner <jenhausu@icloud.com>
2014-12-25 04:50:10 +0800

    建立新檔

---
+++ SleepTracker/SleepTracker/SetAlarm.m
@@ -0,0 +1,13 @@
+//
+//  SetAlarm.m
+//  SleepTracker
+//
+//  Created by 蘇健豪1 on 2014/12/25.
+//  Copyright (c) 2014年 蘇健豪. All rights reserved.
+//
+
+#import "SetAlarm.h"
+
+@implementation SetAlarm
+
+@end
---
+++ SleepTracker/SleepTracker/SetIntelligentNotification.m
@@ -0,0 +1,13 @@
+//
+//  SetIntelligentNotification.m
+//  SleepTracker
+//
+//  Created by 蘇健豪1 on 2014/12/25.
+//  Copyright (c) 2014年 蘇健豪. All rights reserved.
+//
+
+#import "SetIntelligentNotification.h"
+
+@implementation SetIntelligentNotification
+
+@end
