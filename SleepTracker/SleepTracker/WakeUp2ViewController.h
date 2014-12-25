10844feb4ffbf6b4782f613b7a63f07bdc87caec
iOSBeginner <jenhausu@icloud.com>
2014-12-26 03:00:34 +0800

    新增 WakeUp2ViewController 檔案

---
+++ SleepTracker/SleepTracker/WakeUp2ViewController.h
@@ -0,0 +1,13 @@
+//
+//  WakeUp2ViewController.h
+//  SleepTracker
+//
+//  Created by 蘇健豪1 on 2014/12/26.
+//  Copyright (c) 2014年 蘇健豪. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WakeUp2ViewController : UIViewController
+
+@end
