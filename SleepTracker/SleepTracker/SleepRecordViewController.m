b45f602d536b28505f39fbc1e8d9baf1e49e3dd2
Su, Jian-Hau <jenhausu@icloud.com>
2015-01-20 04:55:28 +0800

    將 AlarmViewController 轉移到 SleepRecordViewController

--- SleepTracker/SleepTracker/SleepRecordViewController.m
+++ SleepTracker/SleepTracker/SleepRecordViewController.m
@@ -8,30 +8,148 @@
 
 #import "SleepRecordViewController.h"
 
+#import "SleepDataModel.h"
+#import "SleepData.h"
+
 @interface SleepRecordViewController ()
+
+@property (weak, nonatomic) IBOutlet UIDatePicker *datePicker;
+@property (weak, nonatomic) IBOutlet UILabel *alreadySleptLabel;
+@property (weak, nonatomic) IBOutlet UILabel *alreadyAwakeLabel;
+@property (weak, nonatomic) IBOutlet UIButton *button;
+
+@property (nonatomic, strong) NSTimer *timer;
+
+@property (nonatomic, strong) SleepDataModel *sleepDataModel;
+
+@property (nonatomic, strong) SleepData *sleepData;
+@property (nonatomic, strong) NSArray *fetchDataArray;
 
 @end
 
 @implementation SleepRecordViewController
 
+
+@synthesize timer, fetchDataArray;
+
+- (SleepDataModel *)sleepDataModel
+{
+    if (!_sleepDataModel) {
+        _sleepDataModel = [[SleepDataModel alloc] init];
+    }
+    
+    return _sleepDataModel;
+}
+
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    self.datePicker.date = [NSDate dateWithTimeIntervalSinceNow: 8 * 60 * 60];   //預設鬧鐘是八個小時之後
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)viewWillAppear:(BOOL)animated
+{
+    fetchDataArray = [self.sleepDataModel fetchSleepDataSortWithAscending:NO];
+    if ([fetchDataArray count]) {  //避免一開始完全沒有任何資
+        self.sleepData = fetchDataArray[0];
+        if (self.sleepData.wakeUpTime)  //awake state
+        {
+            [self startCountingAwakeTime];
+            [self.button setTitle:@"上床" forState:UIControlStateNormal];
+        }
+        else  //sleep state
+        {
+            [self startCountingSleepTime];
+            [self.button setTitle:@"起床" forState:UIControlStateNormal];
+        }
+    } else {
+        [self.button setTitle:@"上床" forState:UIControlStateNormal];
+    }
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (IBAction)buttonPress:(UIButton *)sender {
+    if ([sender.titleLabel.text isEqualToString:@"上床"])
+    {
+        [self.button setTitle:@"起床" forState:UIControlStateNormal];
+        self.alreadyAwakeLabel.text = @"00:00:00";
+        
+        [self.sleepDataModel addNewData];
+        fetchDataArray = [self.sleepDataModel fetchSleepDataSortWithAscending:NO];
+        [self.sleepDataModel updateAllSleepDataInRow:fetchDataArray.count - 1
+                                         goToBedTime:[NSDate date] wakeUpTime:nil sleepTiem:nil sleepType:nil];
+        NSInteger const LATEST_DATA = 0;
+        self.sleepData = fetchDataArray[LATEST_DATA];
+        
+        [self stopTimer];
+        [self startCountingSleepTime];
+        
+        
+        /*[self.intelligentNotification deleteIntelligentNotification];
+         [self.customNotification cancelCustomNotification];
+         [self.alarm setAlarm:self.datePicker.date];*/
+    }else {
+        UINavigationController *page2 = [self.storyboard instantiateViewControllerWithIdentifier:@"wakeUpPage"];
+        [self presentViewController:page2 animated:YES completion:nil];
+        
+        NSArray *viewControllers = page2.viewControllers;  //使用一個NSArray來取得UINavigationController下的所有viewControllers，只後再判斷誰是rootViewController
+        UIViewController *rootViewController = [viewControllers objectAtIndex:[viewControllers count] - 1];  //??
+        [rootViewController setValue:self forKey:@"delegate"];  //將delegate設成自己（指定自己為代理
+    }
 }
-*/
+
+- (void)saveButtonPress
+{
+    [self.button setTitle:@"上床" forState:UIControlStateNormal];
+    self.alreadySleptLabel.text = @"00:00:00";
+    
+    [self stopTimer];
+    [self startCountingAwakeTime];
+    
+    /*[self.customNotification setCustomNotification];
+     [self.alarm cancelAlarm];*/
+}
+
+- (void)startCountingSleepTime
+{
+    timer = [NSTimer scheduledTimerWithTimeInterval:1.0f
+                                             target:self
+                                           selector:@selector(updateTime)
+                                           userInfo:@"Go To Bed"
+                                            repeats:YES];
+}
+
+- (void)startCountingAwakeTime
+{
+    timer = [NSTimer scheduledTimerWithTimeInterval:1.0f
+                                             target:self
+                                           selector:@selector(updateTime)
+                                           userInfo:@"Wake Up"
+                                            repeats:YES];
+}
+
+- (void)updateTime
+{
+    if ([timer.userInfo isEqualToString:@"Go To Bed"]) {
+        self.alreadySleptLabel.text = [self stringFromTimeInterval:-[self.sleepData.goToBedTime timeIntervalSinceNow]];  //即時顯示已經睡了多久時間
+    }else {
+        self.alreadyAwakeLabel.text = [self stringFromTimeInterval:-[self.sleepData.wakeUpTime timeIntervalSinceNow]];  //即時顯示已經醒了多久
+    }
+}
+
+- (void)stopTimer
+{
+    [timer invalidate];
+    timer = nil;
+}
+
+- (NSString *)stringFromTimeInterval:(NSTimeInterval)interval
+{
+    NSInteger time = (NSInteger)interval;
+    NSInteger seconds = time % 60; // ％取餘數
+    NSInteger minutes = (time / 60) % 60;
+    NSInteger hours = (time / 3600);
+    return [NSString stringWithFormat:@"%02li:%02li:%02li", (long)hours, (long)minutes, (long)seconds];
+}
 
 @end
