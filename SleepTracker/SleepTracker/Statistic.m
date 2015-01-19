e349b38cded19e2c3cd73223b3022b809243c7ab
Su, Jian-Hau <jenhausu@icloud.com>
2015-01-20 00:25:43 +0800

    新增檔案

---
+++ SleepTracker/SleepTracker/Statistic.m
@@ -0,0 +1,13 @@
+//
+//  Statistic.m
+//  SleepTracker
+//
+//  Created by 蘇健豪1 on 2015/1/20.
+//  Copyright (c) 2015年 蘇健豪. All rights reserved.
+//
+
+#import "Statistic.h"
+
+@implementation Statistic
+
+@end
