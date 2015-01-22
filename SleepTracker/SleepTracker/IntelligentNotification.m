d46884d74cf3402ff8cdc7bed2512ab5ec81006c
Su, Jian-Hau <jenhausu@icloud.com>
2015-01-23 02:29:58 +0800

    重新排程 Intelligent Notification

--- SleepTracker/SleepTracker/IntelligentNotification.m
+++ SleepTracker/SleepTracker/IntelligentNotification.m
@@ -8,7 +8,47 @@
 
 #import "IntelligentNotification.h"
 
+#import "LocalNotification.h"
+#import "SleepDataModel.h"
+#import "SleepData.h"
+
+@interface IntelligentNotification ()
+
+@property (strong, nonatomic) NSArray *notification;
+@property (strong, nonatomic) NSArray *message;
+@property (strong, nonatomic) NSArray *fireTime;
+@property (strong, nonatomic) NSDate *shouldGoToBedTime;
+
+@property (strong, nonatomic) LocalNotification *localNotification;
+@property (strong, nonatomic) SleepDataModel *sleepDataModel;
+@property (strong, nonatomic) NSArray *fetchArray;
+@property (strong, nonatomic) SleepData *sleepData;
+
+@end
+
 @implementation IntelligentNotification
+
+@synthesize notification, message, fireTime, shouldGoToBedTime, fetchArray;
+
+#pragma mark - Lazy initialization
+
+- (LocalNotification *)localNotification
+{
+    if (!_localNotification) {
+        _localNotification = [[LocalNotification alloc] init];
+    }
+    return _localNotification;
+}
+
+- (SleepDataModel *)sleepDataModel
+{
+    if (!_sleepDataModel) {
+        _sleepDataModel = [[SleepDataModel alloc] init];
+    }
+    return _sleepDataModel;
+}
+
+#pragma mark - Decide
 
 - (NSDate *)decideShouldGoToBedTime
 {
@@ -32,7 +72,21 @@
 
 - (void)rescheduleIntelligentNotification
 {
+    [self deleteAllIntelligentNotification];
     
+    [self decideMessage];
+    [self decideFireDate];
+    NSUserDefaults *userPreferences = [NSUserDefaults standardUserDefaults];
+    
+    for (NSInteger i = 0 ; i < [notification count] ; i++ ) {
+        if ([userPreferences boolForKey:notification[i]]) {
+            [self.localNotification setLocalNotificationWithMessage:message[i]
+                                                           fireDate:fireTime[i]
+                                                        repeatOrNot:YES
+                                                              Sound:@"UILocalNotificationDefaultSoundName"
+                                                                key:nil forValue:nil];
+        }
+    }
 }
 
 #pragma mark - Delete
