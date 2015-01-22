b2ddb7ccd5f02adc136853d9ef002bd05dbd5f6e
Su, Jian-Hau <jenhausu@icloud.com>
2015-01-22 18:03:23 +0800

    設定基本架構

--- SleepTracker/SleepTracker/IntelligentNotification.m
+++ SleepTracker/SleepTracker/IntelligentNotification.m
@@ -10,4 +10,34 @@
 
 @implementation IntelligentNotification
 
+- (NSDate *)decideShouldGoToBedTime
+{
+    NSDateComponents *components = [[NSDateComponents alloc] init];
+    [components setHour:23];
+    [components setMinute:0];
+    NSCalendar *calendar = [[NSCalendar alloc] initWithCalendarIdentifier:NSCalendarIdentifierGregorian];  //NSCalendarIdentifierGregorian
+    NSDate *shouldGoToBedTime = [calendar dateFromComponents:components];
+
+    
+    return shouldGoToBedTime;
+}
+
+- (NSArray *)decideFireTime
+{
+    
+    NSArray *array;
+
+    return array;
+}
+
+- (void)rescheduleIntelligentNotification
+{
+    
+}
+
+- (void)deleteIntelligentNotification
+{
+    
+}
+
 @end
