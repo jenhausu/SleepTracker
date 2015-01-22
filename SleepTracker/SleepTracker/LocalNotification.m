b1a0c2e5fd6a732918ca5d098e1ad7142dfcf589
Su, Jian-Hau <jenhausu@icloud.com>
2015-01-22 17:52:28 +0800

    新增檔案，LocalNotificaion

---
+++ SleepTracker/SleepTracker/LocalNotification.m
@@ -0,0 +1,13 @@
+//
+//  LocalNotification.m
+//  SleepTracker
+//
+//  Created by 蘇健豪1 on 2015/1/22.
+//  Copyright (c) 2015年 蘇健豪. All rights reserved.
+//
+
+#import "LocalNotification.h"
+
+@implementation LocalNotification
+
+@end
