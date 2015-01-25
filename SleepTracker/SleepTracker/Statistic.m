709bac61b472dc6a44cba1a772fec57b1bcef489
Su, Jian-Hau <jenhausu@icloud.com>
2015-01-25 15:19:19 +0800

    簡化程式碼

--- SleepTracker/SleepTracker/Statistic.m
+++ SleepTracker/SleepTracker/Statistic.m
@@ -40,108 +40,108 @@
     AVG = 0;
     
     fetchArray = [self.sleepDataModel fetchSleepDataSortWithAscending:NO];
-    
+    if (fetchArray.count > 0 ) {
+        self.sleepData = fetchArray[0];
+    }
 }
 
 - (NSArray *)showSleepTimeDataInTheRecent:(NSInteger)recent
 {
     [self Initailize];
     
-    if (fetchArray.count > 0 ) {
-        self.sleepData = fetchArray[0];
-        if (fetchArray.count >= 2 || (fetchArray.count == 1 && self.sleepData.wakeUpTime > 0) )
-        {
-            NSInteger row = ([self.sleepData.sleepTime floatValue] == 0) ? 1 : 0 ;  //如果現在是睡覺狀態，那就跳過第一筆資料，因為第一筆資料還沒有sleepTime的資料
-            self.sleepData = fetchArray[row];  //如果現在是睡覺狀態，那就跳過第一筆資料，因為第一筆資料還沒有sleepTime的資料
-            NSInteger sleepTime;
-            
-            NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
-            [formatter setDateFormat:@"DDD"];  // 1~366 一年的第幾天
-            
-            NSInteger today = [[formatter stringFromDate:[NSDate date]] integerValue];
-            NSInteger dataDate = [[formatter stringFromDate:self.sleepData.wakeUpTime] integerValue];
-            NSInteger lastDataDate = dataDate + 1 ;
-
-            NSInteger lastMinDate = dataDate + 1;
-            NSMutableArray *lastMinStack = [[NSMutableArray alloc] init];
-
-            NSInteger sleepTimeSum = 0;
-            NSInteger sleepTimeSumTem = 0;
-            NSInteger lastDataSleepTime = 0;
-            
-            NSInteger Correction = (today != dataDate) ? (today - dataDate) : 0 ;
-            
-            while ( dataDate > (today - recent) ) {
-                if ([self.sleepData.sleepType isEqualToString:@"一般"])
-                {
-                    sleepTime = [self.sleepData.sleepTime integerValue];
-                    sleepTimeSum += sleepTime;
+    if (fetchArray.count >= 2 || (fetchArray.count == 1 && self.sleepData.wakeUpTime > 0) )
+    {
+        NSInteger row = ([self.sleepData.sleepTime floatValue] == 0) ? 1 : 0 ;  //如果現在是睡覺狀態，那就跳過第一筆資料，因為第一筆資料還沒有sleepTime的資料
+        self.sleepData = fetchArray[row];  //如果現在是睡覺狀態，那就跳過第一筆資料，因為第一筆資料還沒有sleepTime的資料
+        NSInteger sleepTime;
+        
+        NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+        [formatter setDateFormat:@"DDD"];  // 1~366 一年的第幾天
+        
+        NSInteger today = [[formatter stringFromDate:[NSDate date]] integerValue];
+        NSInteger dataDate = [[formatter stringFromDate:self.sleepData.wakeUpTime] integerValue];
+        NSInteger lastDataDate = dataDate + 1 ;
+        
+        NSInteger lastMinDate = dataDate + 1;
+        NSMutableArray *lastMinStack = [[NSMutableArray alloc] init];
+        
+        NSInteger sleepTimeSum = 0;
+        NSInteger sleepTimeSumTem = 0;
+        NSInteger lastDataSleepTime = 0;
+        
+        NSInteger Correction = (today != dataDate) ? (today - dataDate) : 0 ;
+        
+        while ( dataDate > (today - recent) ) {
+            if ([self.sleepData.sleepType isEqualToString:@"一般"])
+            {
+                sleepTime = [self.sleepData.sleepTime integerValue];
+                sleepTimeSum += sleepTime;
+                
+                if (dataDate != lastDataDate) {
+                    sleepTimeSumTem = 0;  //歸零
                     
-                    if (dataDate != lastDataDate) {
-                        sleepTimeSumTem = 0;  //歸零
+                    if (sleepTime > MAX) {
+                        MAX = sleepTime;
+                    }
+                    
+                    if (sleepTime < MIN) {
+                        MIN = sleepTime;
                         
-                        if (sleepTime > MAX) {
-                            MAX = sleepTime;
-                        }
-                        
-                        if (sleepTime < MIN) {
-                            MIN = sleepTime;
-                            
-                            lastMinDate = dataDate;
-                            [lastMinStack addObject:[NSNumber numberWithFloat:sleepTime]];  //加入堆疊中
-                        }
-                    } else if (dataDate == lastDataDate) {  //兩筆資料是同一天
-                        sleepTimeSumTem += sleepTime + lastDataSleepTime;
-                        
-                        if (sleepTimeSumTem > MAX) {  //處理最大值
-                            MAX = sleepTimeSumTem;
-                        }
-                        
-                        if (lastMinDate == dataDate) {  //處理最小值
-                            if (lastMinStack.count >= 2) {   //堆疊數量超過一個
-                                if (sleepTimeSumTem < [lastMinStack[lastMinStack.count -2] integerValue]) {
-                                    MIN = sleepTimeSumTem;
-                                    lastMinDate = dataDate;
-                                    
-                                    [lastMinStack removeLastObject];
-                                    [lastMinStack addObject:[NSNumber numberWithInteger:sleepTimeSumTem]];
-                                } else {
-                                    MIN = [lastMinStack[lastMinStack.count - 2] integerValue];
-                                    lastMinDate = dataDate;
-                                    
-                                    [lastMinStack removeLastObject];
-                                }
-                            } else {
+                        lastMinDate = dataDate;
+                        [lastMinStack addObject:[NSNumber numberWithFloat:sleepTime]];  //加入堆疊中
+                    }
+                } else if (dataDate == lastDataDate) {  //兩筆資料是同一天
+                    sleepTimeSumTem += sleepTime + lastDataSleepTime;
+                    
+                    if (sleepTimeSumTem > MAX) {  //處理最大值
+                        MAX = sleepTimeSumTem;
+                    }
+                    
+                    if (lastMinDate == dataDate) {  //處理最小值
+                        if (lastMinStack.count >= 2) {   //堆疊數量超過一個
+                            if (sleepTimeSumTem < [lastMinStack[lastMinStack.count -2] integerValue]) {
                                 MIN = sleepTimeSumTem;
                                 lastMinDate = dataDate;
                                 
                                 [lastMinStack removeLastObject];
                                 [lastMinStack addObject:[NSNumber numberWithInteger:sleepTimeSumTem]];
+                            } else {
+                                MIN = [lastMinStack[lastMinStack.count - 2] integerValue];
+                                lastMinDate = dataDate;
+                                
+                                [lastMinStack removeLastObject];
                             }
+                        } else {
+                            MIN = sleepTimeSumTem;
+                            lastMinDate = dataDate;
+                            
+                            [lastMinStack removeLastObject];
+                            [lastMinStack addObject:[NSNumber numberWithInteger:sleepTimeSumTem]];
                         }
                     }
-                    
-                    lastDataSleepTime = sleepTime;
-                    lastDataDate = dataDate;
-                    
-                    if (lastDataDate - dataDate > 1) {  //如果中間有一天是沒有輸入資料的話進行校正，中間這幾天不納入計算
-                        Correction += (lastDataDate - dataDate) - 1;
-                    }
                 }
-                if (++row < fetchArray.count) {
-                    self.sleepData = fetchArray[row];
-                    dataDate = [[formatter stringFromDate:self.sleepData.wakeUpTime] integerValue];
-                } else {
-                    break;  //如果總資料比數少於所需要計算的天數，直接跳出
+                
+                lastDataSleepTime = sleepTime;
+                lastDataDate = dataDate;
+                
+                if (lastDataDate - dataDate > 1) {  //如果中間有一天是沒有輸入資料的話進行校正，中間這幾天不納入計算
+                    Correction += (lastDataDate - dataDate) - 1;
                 }
             }
-            AVG = sleepTimeSum / (today - lastDataDate + 1 - Correction);
+            if (++row < fetchArray.count) {
+                self.sleepData = fetchArray[row];
+                dataDate = [[formatter stringFromDate:self.sleepData.wakeUpTime] integerValue];
+            } else {
+                break;  //如果總資料比數少於所需要計算的天數，直接跳出
+            }
         }
+        AVG = sleepTimeSum / (today - lastDataDate + 1 - Correction);
     } else {
         MIN = 0;
         MAX = 0;
         AVG = 0;
     }
+    
     if (MIN == 99999999) {
         MIN = 0;
     }
@@ -159,7 +159,18 @@
 - (NSArray *)showWakeUpTimeDataInTheRecent:(NSInteger)recent
 {
     [self Initailize];
-    
+    if (fetchArray.count >= 2 || (fetchArray.count == 1 && self.sleepData.wakeUpTime > 0) )
+    {
+        
+    } else {
+        MIN = 0;
+        MAX = 0;
+        AVG = 0;
+    }
+    if (MIN == 99999999) {
+        MIN = 0;
+    }
+
     return @[[NSNumber numberWithFloat:MIN], [NSNumber numberWithFloat:MAX], [NSNumber numberWithFloat:AVG]];
 }
 
