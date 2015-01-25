33e0642991ac8972a87678909f7d3bbc2a18463b
Su, Jian-Hau <jenhausu@icloud.com>
2015-01-26 03:24:57 +0800

    顯示已經排程的通知

--- SleepTracker/SleepTracker/CustomeNotificationOneTableViewController.m
+++ SleepTracker/SleepTracker/CustomeNotificationOneTableViewController.m
@@ -10,48 +10,36 @@
 
 @interface CustomeNotificationOneTableViewController ()
 
+@property (strong, nonatomic) NSArray *arrayOfAllLocalNotification;
+
 @end
 
 @implementation CustomeNotificationOneTableViewController
 
+@synthesize arrayOfAllLocalNotification;
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    // Uncomment the following line to preserve selection between presentations.
-    // self.clearsSelectionOnViewWillAppear = NO;
-    
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
-}
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    UIApplication *application = [UIApplication sharedApplication];
+    arrayOfAllLocalNotification = [application scheduledLocalNotifications];
 }
 
 #pragma mark - Table view data source
 
-- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-#warning Potentially incomplete method implementation.
-    // Return the number of sections.
-    return 0;
-}
-
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-#warning Incomplete method implementation.
-    // Return the number of rows in the section.
-    return 0;
+    return arrayOfAllLocalNotification.count;
 }
 
-/*
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:<#@"reuseIdentifier"#> forIndexPath:indexPath];
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell" forIndexPath:indexPath];
     
-    // Configure the cell...
+    UILocalNotification *localNotification = arrayOfAllLocalNotification[indexPath.row];
+    cell.textLabel.text = localNotification.alertBody;
     
     return cell;
 }
-*/
+
 
 /*
 // Override to support conditional editing of the table view.
