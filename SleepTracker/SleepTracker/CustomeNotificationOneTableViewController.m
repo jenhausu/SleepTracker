43543476cb29cdda5ec74faf994b19c51a3349aa
Su, Jian-Hau <jenhausu@icloud.com>
2015-01-26 22:10:05 +0800

    刪除不需要的程式碼

--- SleepTracker/SleepTracker/CustomeNotificationOneTableViewController.m
+++ SleepTracker/SleepTracker/CustomeNotificationOneTableViewController.m
@@ -84,34 +84,10 @@
         // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
     }   
 }
-*/
+
 - (IBAction)add:(id)sender {
     UINavigationController *page2 = [self.storyboard instantiateViewControllerWithIdentifier:@"setCustomNotification"];
     [self presentViewController:page2 animated:YES completion:nil];
 }
-
-/*
-// Override to support rearranging the table view.
-- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
-}
-*/
-
-/*
-// Override to support conditional rearranging of the table view.
-- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
-    // Return NO if you do not want the item to be re-orderable.
-    return YES;
-}
-*/
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
