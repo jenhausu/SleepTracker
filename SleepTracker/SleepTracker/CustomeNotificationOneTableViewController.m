7cfb03f62ec6a451ff05e7d304284e549cdd18b4
Su, Jian-Hau <jenhausu@icloud.com>
2015-01-26 22:10:33 +0800

    設定可以滑動刪除 CustomNotification

--- SleepTracker/SleepTracker/CustomeNotificationOneTableViewController.m
+++ SleepTracker/SleepTracker/CustomeNotificationOneTableViewController.m
@@ -66,21 +66,30 @@
 }
 
 
-/*
-// Override to support conditional editing of the table view.
 - (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
-    // Return NO if you do not want the specified item to be editable.
     return YES;
 }
-*/
 
-/*
-// Override to support editing the table view.
 - (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
-    if (editingStyle == UITableViewCellEditingStyleDelete) {
-        // Delete the row from the data source
+    if (editingStyle == UITableViewCellEditingStyleDelete)
+    {
+        for (NSInteger row = 0 ; row < CustomNotification.count ; row++ )
+        {
+            localNotification = CustomNotification[row];
+            
+            if (row == indexPath.row) {
+                UIApplication *application = [UIApplication sharedApplication];
+                [application cancelLocalNotification:localNotification];
+                
+                [CustomNotification removeObjectAtIndex:row];
+                
+                break;
+            }
+        }
+        
         [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
+    }
+    else if (editingStyle == UITableViewCellEditingStyleInsert) {
         // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
     }   
 }
