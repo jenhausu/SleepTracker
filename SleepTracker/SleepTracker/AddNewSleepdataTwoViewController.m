52253831f7460dbf99ef96eff8d531836fb99252
Su, Jian-Hau <jenhausu@icloud.com>
2015-01-30 21:01:48 +0800

    新增檔案（AddNewSleepdataTableViewController, AddNewSleepdataTwoViewController）

---
+++ SleepTracker/SleepTracker/AddNewSleepdataTwoViewController.m
@@ -0,0 +1,37 @@
+//
+//  AddNewSleepdataTwoViewController.m
+//  SleepTracker
+//
+//  Created by 蘇健豪1 on 2015/1/30.
+//  Copyright (c) 2015年 蘇健豪. All rights reserved.
+//
+
+#import "AddNewSleepdataTwoViewController.h"
+
+@interface AddNewSleepdataTwoViewController ()
+
+@end
+
+@implementation AddNewSleepdataTwoViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
