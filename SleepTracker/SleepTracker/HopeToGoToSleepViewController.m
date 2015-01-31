168f27508edc954d6d793853371fa6bf075b0210
Su, Jian-Hau <jenhausu@icloud.com>
2015-02-01 01:50:37 +0800

    新增檔案（HopeToGoToSleep）

---
+++ SleepTracker/SleepTracker/HopeToGoToSleepViewController.m
@@ -0,0 +1,22 @@
+//
+//  HopeToGoToSleepViewController.m
+//  SleepTracker
+//
+//  Created by 蘇健豪1 on 2015/2/1.
+//  Copyright (c) 2015年 蘇健豪. All rights reserved.
+//
+
+#import "HopeToGoToSleepViewController.h"
+
+@interface HopeToGoToSleepViewController ()
+
+@end
+
+@implementation HopeToGoToSleepViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+@end
