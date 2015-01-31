5bd8c7eb8c151ff6fe04933632c36e032b871f3e
Su, Jian-Hau <jenhausu@icloud.com>
2015-01-31 16:57:47 +0800

    優化傳值方式

--- SleepTracker/SleepTracker/AddNewSleepdataTwoViewController.m
+++ SleepTracker/SleepTracker/AddNewSleepdataTwoViewController.m
@@ -10,43 +10,62 @@
 
 #import "AddNewSleepdataTwoViewController.h"
 
+#import "SleepDataModel.h"
+#import "SleepData.h"
+
 @interface AddNewSleepdataTwoViewController ()
 
 @property (weak, nonatomic) IBOutlet UIDatePicker *datePicker;
-@property (weak, nonatomic) IBOutlet UILabel *label;
+@property (weak, nonatomic) IBOutlet UILabel *dateLabel;
 
 @property (strong, nonatomic) NSDateFormatter *dateFormatter;
 
-@property (strong, nonatomic) NSDate *receiveDate;
+@property (strong, nonatomic) NSString *DateType;
+@property (strong, nonatomic) NSDate *goToBedTime;
+@property (strong, nonatomic) NSDate *wakeUpTime;
 @property (strong, nonatomic) AddNewSleepdataTwoViewController *addNewSleepdataTwoViewController;
+
+@property (strong, nonatomic) SleepDataModel *sleepDataModel;
+@property (strong, nonatomic) SleepData *sleepData;
+@property (strong, nonatomic) NSArray *fetchDataArray;
 
 @end
 
 @implementation AddNewSleepdataTwoViewController
 
-@synthesize dateFormatter, receiveDate;
+@synthesize dateFormatter, DateType, goToBedTime, wakeUpTime, fetchDataArray;
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     
     dateFormatter = [[NSDateFormatter alloc] init];
     [dateFormatter setDateFormat:@"u/MM/dd EEE ahh:mm"];
-    self.label.text = [dateFormatter stringFromDate:receiveDate];
     
-    self.datePicker.date = receiveDate;
+    self.sleepDataModel = [[SleepDataModel alloc] init];
+    fetchDataArray = [self.sleepDataModel fetchSleepDataSortWithAscending:NO];
+    if ([DateType isEqualToString:@"goToBedTime"])
+    {
+        self.datePicker.date = goToBedTime;
+        self.dateLabel.text = [dateFormatter stringFromDate:goToBedTime];
+    }
+    else if ([DateType isEqualToString:@"wakeUpTime"])
+    {
+        self.datePicker.date = wakeUpTime;
+        self.dateLabel.text = [dateFormatter stringFromDate:wakeUpTime];
+    }
 }
 
 - (IBAction)valueChanged:(id)sender {
-    self.label.text = [dateFormatter stringFromDate:self.datePicker.date];
+    self.dateLabel.text = [dateFormatter stringFromDate:self.datePicker.date];
 }
 
 - (void)willMoveToParentViewController:(UIViewController *)parent
 {
     if (!parent) {
-        if ([self.title isEqualToString:@"上床時間"]) {
+        if ([DateType isEqualToString:@"goToBedTime"]) {
             [self.addNewSleepdataTwoViewController setValue:self.datePicker.date forKey:@"goToBedTime"];
         }
-        else if ([self.title isEqualToString:@"起床時間"]) {
+        else if ([DateType isEqualToString:@"wakeUpTime"]) {
             [self.addNewSleepdataTwoViewController setValue:self.datePicker.date forKey:@"wakeUpTime"];
         }
     }
