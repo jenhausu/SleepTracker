61fa19b9cea2a42bda10e47c5a73089b5ad74abb
Su, Jian-Hau <jenhausu@icloud.com>
2015-02-12 12:10:34 +0800

    新增 SettingTableViewController 檔案

---
+++ SleepTracker/SleepTracker/SettingTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  SettingTableViewController.h
+//  SleepTracker
+//
+//  Created by 蘇健豪1 on 2015/2/12.
+//  Copyright (c) 2015年 蘇健豪. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingTableViewController : UITableViewController
+
+@end
