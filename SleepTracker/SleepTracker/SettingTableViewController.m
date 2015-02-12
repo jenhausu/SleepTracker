6c340fad63e57e8d00cfe4b51848c8197964a0e5
Su, Jian-Hau <jenhausu@icloud.com>
2015-02-12 13:22:02 +0800

    將設定頁面改成用程式顯示，並且設定基本顧客意見回饋欄（寄 E-mail 給我）

--- SleepTracker/SleepTracker/SettingTableViewController.m
+++ SleepTracker/SleepTracker/SettingTableViewController.m
@@ -8,93 +8,119 @@
 
 #import "SettingTableViewController.h"
 
-@interface SettingTableViewController ()
+#import <MessageUI/MessageUI.h>
+
+@interface SettingTableViewController ()  <MFMailComposeViewControllerDelegate>
+
+@property (strong, nonatomic) NSArray *section1;
+@property (strong, nonatomic) NSArray *section2;
+@property (strong, nonatomic) NSArray *section3;
+@property (strong, nonatomic) NSArray *textLabelOfTableViewCell;
 
 @end
 
 @implementation SettingTableViewController
 
+@synthesize section1, section2, section3, textLabelOfTableViewCell;
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    // Uncomment the following line to preserve selection between presentations.
-    // self.clearsSelectionOnViewWillAppear = NO;
-    
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
-}
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    section1 = @[@"希望床時間", @"睡前通知"];
+    section2 = @[@"意見回饋"];
+    section3 = @[@"使用說明"];
+    textLabelOfTableViewCell = @[section1, section2, section3];
 }
 
 #pragma mark - Table view data source
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-#warning Potentially incomplete method implementation.
-    // Return the number of sections.
-    return 0;
+    return textLabelOfTableViewCell.count;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-#warning Incomplete method implementation.
-    // Return the number of rows in the section.
-    return 0;
+    return [textLabelOfTableViewCell[section] count];
 }
 
-/*
+
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:<#@"reuseIdentifier"#> forIndexPath:indexPath];
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell" forIndexPath:indexPath];
     
-    // Configure the cell...
+    cell.textLabel.text = textLabelOfTableViewCell[indexPath.section][indexPath.row];
+    
+    if (indexPath.section == 0) {
+        cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+    }
     
     return cell;
 }
-*/
 
-/*
-// Override to support conditional editing of the table view.
-- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
-    // Return NO if you do not want the specified item to be editable.
-    return YES;
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UIViewController *page2;
+    
+    if (indexPath.section == 0) {
+        if (indexPath.row == 0) {
+            page2 = [self.storyboard instantiateViewControllerWithIdentifier:@"HopeToGoToBedPage"];
+        } else if (indexPath.row == 1) {
+            page2 = [self.storyboard instantiateViewControllerWithIdentifier:@"SleepNotificationPage"];
+        }
+        
+        [self.navigationController pushViewController:page2 animated:YES];
+    } else if (indexPath.section == 1) {
+        if (indexPath.row == 0)
+        {
+            if ([MFMailComposeViewController canSendMail])
+            {
+                NSString *emailTitle = @"Title";
+                NSString *messageBody = @"Message Body";
+                NSArray *toRecipents = [NSArray arrayWithObject:@"jenhausu@icloud.com"];
+                
+                MFMailComposeViewController *mailViewController = [[MFMailComposeViewController alloc] init];
+                mailViewController.mailComposeDelegate = self;
+                [mailViewController setSubject:emailTitle];
+                [mailViewController setMessageBody:messageBody isHTML:YES];
+                [mailViewController setToRecipients:toRecipents];
+                
+                [self presentViewController:mailViewController animated:YES completion:NULL];
+            } else {
+                UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Failure"
+                                                                message:@"Your device doesn't support the composer sheet"
+                                                               delegate:nil
+                                                      cancelButtonTitle:@"OK"
+                                                      otherButtonTitles:nil];
+                [alert show];
+            }
+
+            
+        }
+    } else if (indexPath.section == 2) {
+        if (indexPath.row == 0) {
+            
+        }
+    }
 }
-*/
 
-/*
-// Override to support editing the table view.
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
-    if (editingStyle == UITableViewCellEditingStyleDelete) {
-        // Delete the row from the data source
-        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-    }   
+- (void) mailComposeController:(MFMailComposeViewController *)controller didFinishWithResult:(MFMailComposeResult)result error:(NSError *)error
+{
+    switch (result)
+    {
+        case MFMailComposeResultCancelled:
+            NSLog(@"Mail cancelled");
+            break;
+        case MFMailComposeResultSaved:
+            NSLog(@"Mail saved");
+            break;
+        case MFMailComposeResultSent:
+            NSLog(@"Mail sented");
+            break;
+        case MFMailComposeResultFailed:
+            NSLog(@"Mail sent failure: %@", [error localizedDescription]);
+            break;
+        default:
+            break;
+    }
+    [self dismissViewControllerAnimated:YES completion:NULL];
 }
-*/
-
-/*
-// Override to support rearranging the table view.
-- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
-}
-*/
-
-/*
-// Override to support conditional rearranging of the table view.
-- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
-    // Return NO if you do not want the item to be re-orderable.
-    return YES;
-}
-*/
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
