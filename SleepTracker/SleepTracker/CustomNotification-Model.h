869918d6e4c3401d2b1b70224c9ff8318ef0f0e8
Su, Jian-Hau <jenhausu@icloud.com>
2015-02-27 15:47:08 +0800

    新增 CustomNotification_Model 檔案

---
+++ SleepTracker/SleepTracker/CustomNotification-Model.h
@@ -0,0 +1,13 @@
+//
+//  CustomNotification-Model.h
+//  SleepTracker
+//
+//  Created by 蘇健豪1 on 2015/2/27.
+//  Copyright (c) 2015年 蘇健豪. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CustomNotification_Model : NSObject
+
+@end
---
+++ SleepTracker/SleepTracker/CustomNotification-Model.m
@@ -0,0 +1,13 @@
+//
+//  CustomNotification-Model.m
+//  SleepTracker
+//
+//  Created by 蘇健豪1 on 2015/2/27.
+//  Copyright (c) 2015年 蘇健豪. All rights reserved.
+//
+
+#import "CustomNotification-Model.h"
+
+@implementation CustomNotification_Model
+
+@end
