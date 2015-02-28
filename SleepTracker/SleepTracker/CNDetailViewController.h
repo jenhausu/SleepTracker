f2752724371a9b1c465d74d51a0aafa07a1b3429
Su, Jian-Hau <jenhausu@icloud.com>
2015-02-28 17:16:32 +0800

    新增檔案

---
+++ SleepTracker/SleepTracker/CNDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  CNDetailViewController.h
+//  SleepTracker
+//
+//  Created by 蘇健豪1 on 2015/2/28.
+//  Copyright (c) 2015年 蘇健豪. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CNDetailViewController : UIViewController
+
+@end
---
+++ SleepTracker/SleepTracker/CNDetailViewController.m
@@ -0,0 +1,22 @@
+//
+//  CNDetailViewController.m
+//  SleepTracker
+//
+//  Created by 蘇健豪1 on 2015/2/28.
+//  Copyright (c) 2015年 蘇健豪. All rights reserved.
+//
+
+#import "CNDetailViewController.h"
+
+@interface CNDetailViewController ()
+
+@end
+
+@implementation CNDetailViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+@end
