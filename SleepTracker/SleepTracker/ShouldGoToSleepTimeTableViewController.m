2983fa6bd7a4da8c4be50df101d17e9be55cbc93
Su, Jian-Hau <jenhausu@icloud.com>
2015-03-04 16:54:18 +0800

    新增 ShouldGoToSleepTimeTableViewController 檔案

---
+++ SleepTracker/SleepTracker/ShouldGoToSleepTimeTableViewController.m
@@ -0,0 +1,59 @@
+//
+//  ShouldGoToSleepTimeTableViewController.m
+//  SleepTracker
+//
+//  Created by 蘇健豪1 on 2015/3/4.
+//  Copyright (c) 2015年 蘇健豪. All rights reserved.
+//
+
+#import "ShouldGoToSleepTimeTableViewController.h"
+
+@interface ShouldGoToSleepTimeTableViewController ()
+
+@end
+
+@implementation ShouldGoToSleepTimeTableViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    // Uncomment the following line to preserve selection between presentations.
+    // self.clearsSelectionOnViewWillAppear = NO;
+    
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+#warning Potentially incomplete method implementation.
+    // Return the number of sections.
+    return 0;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+#warning Incomplete method implementation.
+    // Return the number of rows in the section.
+    return 0;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell" forIndexPath:indexPath];
+    
+    // Configure the cell...
+    
+    return cell;
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
